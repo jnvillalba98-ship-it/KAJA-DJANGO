542e3b4c629e7d34595357080fc0930ae293c088
Nicolás <jnvillalba98@gmail.com>
2026-07-20 21:04:58 -0500

    Configura ALLOWED_HOSTS para acceso desde red local

--- kaja/settings.py
+++ kaja/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-5a=6fh11)mlg!d9^o*qqlzqsrn5l^gpe&#uvg#xayd&5dvxb=s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
